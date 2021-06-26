05552482dd59f5de64ba505cb8e3167ff630a00d
Eric Webber <ewebber283@gmail.com>
2021-06-26 14:33:45 -0500

    added user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 
 const apiRoutes = require('./');
 const homeRoutes = require('./home-routes');
+const userRoutes = require('./user-routes');
 const inventoryRoutes = require('./inventory-routes.js');
 
 
 router.use('./api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/users', userRoutes);
 router.use('/inventory', inventoryRoutes);
 
 
