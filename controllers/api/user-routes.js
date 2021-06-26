0283d6dbedf631166dfffe6db38289bb5e5e862b
Eric Webber <ewebber283@gmail.com>
2021-06-26 18:07:34 -0500

    updated routes to add sessions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 
 router.get('/', (req, res) => {
@@ -18,7 +18,21 @@ router.get('/:id', (req, res) => {
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+          {
+            model: Post,
+            attributes: ['id', 'title', 'post_text', 'created_at']
+          },
+          {
+            model: Comment,
+            attributes: ['id', 'body', 'created_at'],
+            include: {
+              model: Post,
+              attributes: ['title']
+            }
+          }
+        ]
     })
     .then(dbUserData => {
         if(!dbUserData) {
@@ -39,13 +53,16 @@ router.post('/', (req, res) => {
       email: req.body.email,
       password: req.body.password
     })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
+          res.json(dbUserData);
+        });
       });
   });
-
   router.post('/login', (req, res) => {
     User.findOne({
       where: {
@@ -62,10 +79,14 @@ router.post('/', (req, res) => {
         res.status(400).json({ message: 'Incorrect password!' });
         return;
       }
-  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
       res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });
+});
 
 
 
