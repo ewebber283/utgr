2b7c54502814301e294ad464b00ffac3ba8ee785
nayala <nathaniel.ayala20@gmail.com>
2021-06-28 18:49:39 -0500

    Started author-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,13 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const authorRoutes = require('./author-routes.js');
 const inventoryRoutes = require('./inventory-routes.js');
 
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/author', authorRoutes);
 router.use('/inventory', inventoryRoutes);
 
 
