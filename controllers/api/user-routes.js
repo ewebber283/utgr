f9328313181a5ef3922a966e3fd70403f2b29d0d
Eric Webber <ewebber283@gmail.com>
2021-06-28 18:40:04 -0500

    updated with users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 
 router.get('/', (req, res) => {
@@ -18,7 +18,21 @@ router.get('/:id', (req, res) => {
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+          {
+            model: Post,
+            attributes: ['id', 'title', 'post_url', 'post_text', 'created_at']
+          },
+          {
+            model: Comment,
+            attributes: ['id', 'body', 'created_at'],
+            include: {
+              model: Post,
+              attributes: ['title']
+            }
+          }
+        ]
     })
     .then(dbUserData => {
         if(!dbUserData) {
@@ -39,13 +53,16 @@ router.post('/', (req, res) => {
       email: req.body.email,
       password: req.body.password
     })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
+          res.json(dbUserData);
+        });
       });
   });
-
   router.post('/login', (req, res) => {
     User.findOne({
       where: {
@@ -62,14 +79,27 @@ router.post('/', (req, res) => {
         res.status(400).json({ message: 'Incorrect password!' });
         return;
       }
-  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
       res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });
+});
 
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+  });
 
-
-  router.put('/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     User.update(req.body, {
         individualHooks: true,
       where: {
@@ -89,7 +119,7 @@ router.post('/', (req, res) => {
   });
 
 
-  router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     User.destroy({
       where: {
         id: req.params.id
