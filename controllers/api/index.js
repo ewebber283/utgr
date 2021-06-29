272bba89a9999f91b3c56727239f91ac719c3d45
Hector Rangel <hector.rangeljr@yahoo.com>
2021-06-28 19:33:49 -0500

    blah

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./');
 const homeRoutes = require('./home-routes');
+const userRoutes = require('./user-routes');
+
 const inventoryRoutes = require('./inventory-routes.js');
 
 
-router.use('./api', apiRoutes);
+//router.use('./api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/author', authorRoutes);
+router.use('/users', userRoutes);
 router.use('/inventory', inventoryRoutes);
 
 
