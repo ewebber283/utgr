58ae49dea75e9d60e57478705ff33bbab261c350
Imerzel <zach.imerzel@gmail.com>
2021-06-30 15:47:04 -0500

    fixed dashboard render and comment render

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,46 +3,45 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 const withAuth = require('../utils/auth');
 
-// get all posts for dashboard
-// router.get('/', withAuth, (req, res) => {
-//   console.log(req.session);
-//   console.log('======================');
-//   Post.findAll({
-//     where: {
-//       user_id: req.session.user_id
-//     },
-//     attributes: [
-//         'id',
-//         'post_url',
-//         'post_text',
-//         'title',
-//         'created_at'
-//     //   [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-//     ],
-//     include: [
-//       {
-//         model: Comment,
-//         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//         include: {
-//           model: User,
-//           attributes: ['username']
-//         }
-//       },
-//       // {
-//       //   model: User,
-//       //   attributes: ['username']
-//       // }
-//     ]
-//   })
-//     .then(dbPostData => {
-//       const posts = dbPostData.map(post => post.get({ plain: true }));
-//       res.render('dashboard', { posts, loggedIn: true });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+router.get('/', withAuth, (req, res) => {
+  console.log(req.session);
+  console.log('======================');
+  Post.findAll({
+    // where: {
+    //   user_id: req.session.user_id
+    // },
+    attributes: [
+        'id',
+        'post_url',
+        'post_text',
+        'title',
+        'created_at'
+    //   [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      // {
+      //   model: User,
+      //   attributes: ['username']
+      // }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.get('/', (req, res) => {
   Comment.findAll({
